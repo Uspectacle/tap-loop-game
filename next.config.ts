dd4194d8f5d5a6703bd8190c33ec29b0953655c1
Uspectacle <super.mario.larsen@gmail.com>
2025-10-24 11:18:55 +0200

    fix: :ambulance: Add a prefix for github page

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,14 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig: NextConfig = {
   /* config options here */
   reactCompiler: true,
   outputFileTracingRoot: __dirname,
   output: "export",
+  basePath: isProd ? "/tap-loop-game" : "",
+  assetPrefix: isProd ? "/tap-loop-game/" : "",
 };
 
 export default nextConfig;
