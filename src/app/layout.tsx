6d04b42b1f05f4b586a1e9d63143804fddcb3d84
Uspectacle <super.mario.larsen@gmail.com>
2025-10-26 18:50:44 +0100

    fix: :ambulance: Suspense to fix the useSearchParams error

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import { GameProvider } from "@/components/GameProvider";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import type { Metadata } from "next";
+import { Suspense } from "react";
 import "./Layout.css";
 
 export const metadata: Metadata = {
@@ -18,25 +19,25 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <GameProvider>
-          <div className="layout">
-            <main className="main">
-              <h1>Tap Loop Game</h1>
-              <p className="description">
-                Find the smallest loop taping every block.{" "}
-                <a
-                  href="https://github.com/Uspectacle/tap-loop-game"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="github-link"
-                >
-                  Learn more on <FontAwesomeIcon icon={faGithub} /> Github
-                </a>
-              </p>
-              {children}
-            </main>
-          </div>
-        </GameProvider>
+        <div className="layout">
+          <main className="main">
+            <h1>Tap Loop Game</h1>
+            <p className="description">
+              Find the smallest loop taping every block.{" "}
+              <a
+                href="https://github.com/Uspectacle/tap-loop-game"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="github-link"
+              >
+                Learn more on <FontAwesomeIcon icon={faGithub} /> Github
+              </a>
+            </p>
+            <Suspense fallback={<p>Loading...</p>}>
+              <GameProvider>{children}</GameProvider>
+            </Suspense>
+          </main>
+        </div>
       </body>
     </html>
   );
